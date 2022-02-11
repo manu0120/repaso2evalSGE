b8a528296a6ae94a0f847cae8d946e38193d902a
Manuel <manu0120sp@gmail.com>
2022-02-11 10:18:38 +0100

    new field avatar (Image)

--- odoo14-custom-addons/lista_tareas/models/models.py
+++ odoo14-custom-addons/lista_tareas/models/models.py
@@ -12,11 +12,13 @@ class lista_tareas(models.Model):
     #Los tipos de datos a usar en el ORM son 
     # https://www.odoo.com/documentation/14.0/developer/reference/addons/orm.html#fields
    
+    avatar= fields.Image('Imagen tarea',max_width=50,max_heigth=50)
     tarea = fields.Char()
     prioridad = fields.Integer()
     urgente = fields.Boolean(compute="_value_urgente", store=True)
     realizada = fields.Boolean()
     fecha = fields.Date()
+    
 
     #Este computo depende de la variable prioridad
     @api.depends('prioridad')
